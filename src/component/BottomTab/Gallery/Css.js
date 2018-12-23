351022f240239bcf9a88bd4f91ead0b231d34ecc
hieu tran minh <hieutranminh211@gmail.com>
2018-12-23 22:55:33 +0700

    gallery - init - no set ui, no optimize

--- src/component/BottomTab/Gallery/Css.js
+++ src/component/BottomTab/Gallery/Css.js
@@ -5,5 +5,66 @@ import AppStyles from '../../../Handling/Css'
 export default Css = StyleSheet.create({
     container: {
         flex:1,
-    }
+    },
+    row:{
+        flexDirection:'row',
+        flexWrap: 'wrap'
+      },
+      rowCenter:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+      },
+      image:{
+        width:100,
+        height:100
+      },
+      button: {
+        backgroundColor: 'gray',
+        width: 150,
+        height: 50,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 10
+      },
+      buttonText:{
+        color: 'white'
+      },
+      modal:{
+        height: 200,
+        width: 300,
+        marginTop: 200,
+        padding: 10,
+        alignSelf: 'center',
+        backgroundColor:'lightblue',
+        margin:10,
+        borderRadius:10,
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      closeButton:{
+        alignSelf: 'flex-end'
+      },
+      submitButton:{
+        alignSelf: 'center',
+        backgroundColor: 'darkblue',
+        width: 100,
+        height: 44,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 10
+      },
+      headerText:{
+        fontSize: 20,
+        alignSelf: 'center'
+      },
+      title:{
+        backgroundColor:'gray',
+        padding:5
+      },
+      titleText:{
+        color: 'white'
+      }
 })
--- src/component/BottomTab/Gallery/Gallery.js
+++ src/component/BottomTab/Gallery/Gallery.js
@@ -1,10 +1,60 @@
 import React,{Component} from 'react';
-import {Text,  View} from 'react-native';
+import {Text,  View, Image, TouchableOpacity, Modal, Slider} from 'react-native';
 import firebase from 'react-native-firebase'
-import * as AllHandle from '../../../Handling/AuthHandle'
 import Css from './Css';
 import LinearGradient from 'react-native-linear-gradient'
+import { showImagePicker } from 'react-native-image-picker';
 export default class Gallery extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      rank5:[],
+      rank4:[],
+      rank3:[],
+      rank2:[],
+      rank1:[],
+      image: null,
+      modalVisible:false,
+      value:5
+    };
+  }
+
+  putImageIntoList(){
+    this.setState({
+      ['star'+this.state.value]:[...this.state['Rank' + this.state.value], this.state.image],
+      modalVisible:false,
+      value:5
+    })
+  }
+  takePhoto(){
+    showImagePicker.launchCamera({noData:true}, this.setImage);
+  }
+
+  chooseImage(){
+    showImagePicker.lauchImageLibraly({nodata: true}, this.setImage);
+  }
+  setImage(response){
+    console.log('Response = ', response);
+
+    if (response.didCancel) {
+      console.log('User cancelled image picker');
+    }
+    else if (response.error) {
+      console.log('ImagePicker Error: ', response.error);
+    }
+    else if (response.customButton) {
+      console.log('User tapped custom button: ', response.customButton);
+    } else {
+      //If it is iOS, remove 'file://' prefix
+      let source = {uri: response.uri.replace('file://', ''), isStatic: true};
+
+      //If android, don't need to remove the 'file://'' prefix
+      if (Platform.OS === 'android') {
+        source = {uri: response.uri, isStatic: true};
+      }
+      this.setState({image: source, modalVisible: true});
+    }
+  }
     render(){
         return(
             <LinearGradient
@@ -12,6 +62,7 @@ export default class Gallery extends Component{
                             start={{ x: 1, y: 1 }} 
                             end={{x:0,y:0}} 
              style={Css.container}>
+
 
             </LinearGradient>
         )
